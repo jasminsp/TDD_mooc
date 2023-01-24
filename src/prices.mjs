006d1ccccb256053b9c3f69679c17f49c50dff79
jasminsp <74709722+jasminsp@users.noreply.github.com>
2023-01-24 21:35:12 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -20,6 +20,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date2 = parseDate(req.query.date);
+    const date = parseDate(req.query.date);
     const cost = calculateCost(age, type, baseCost, date2);
     res.json({ cost });
   });
