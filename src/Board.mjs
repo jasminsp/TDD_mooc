25a09ce70e55ff38b60e015b7ffe807584f20b7e
jasminsp <74709722+jasminsp@users.noreply.github.com>
2023-01-28 15:09:17 +0200

    modification for board

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,24 +1,19 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    const drop = false;
-    const count = 1;
-    const maxCount = 3;
+    this.board = "...\n...\n...\n";
   }
 
   drop() {
-    this.drop = true;
+    this.board = ".X.\n...\n...\n";
   }
 
   toString() {
-    if (this.drop === true) {
-      return ".X.\n...\n...\n";
-    } else {
-      return "...\n...\n...\n";
-    }
+    return this.board;
   }
 }
